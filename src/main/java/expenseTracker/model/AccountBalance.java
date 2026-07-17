35648d620e6a0a32071caa47cf4cd188e483017c
Mehmet Tosun <mtosun@student.ethz.ch>
2026-07-17 13:47:43 +0200

    delete inheritance; change structure

--- src/main/java/expenseTracker/model/AccountBalance.java
+++ src/main/java/expenseTracker/model/AccountBalance.java
@@ -1,6 +1,33 @@
 package expenseTracker.model;
 
+import java.math.BigDecimal;
+import java.util.*;
+
 public class AccountBalance {
-    
-    
+    // By default, total amount of expenses are initialized to 0
+    BigDecimal amount = BigDecimal.ZERO;
+
+    // Creates map from unique expense id's to its expense
+    Map<Long, Expense> expenseMap = new HashMap<Long, Expense>();
+
+    public void addExpense(Expense e) {
+        updateAmount();
+        expenseMap.put(e.getUniqueID(), e);
+    }
+
+    public void removeExpense(Expense e) {
+        updateAmount();
+        expenseMap.remove(e.getUniqueID());
+    }
+
+    public void updateAmount() {
+        BigDecimal currAmount = BigDecimal.ZERO;
+
+        for (Long l : expenseMap.keySet()) {
+            Expense currExpense = expenseMap.get(l);
+            currAmount = currAmount.add(currExpense.getAmount());
+        }
+
+        amount = currAmount;
+    }
 }
--- src/main/java/expenseTracker/model/MonthAccountBalance.java
+++
@@ -1,5 +0,0 @@
-package expenseTracker.model;
-
-public class MonthAccountBalance extends AccountBalance {
-    
-}
