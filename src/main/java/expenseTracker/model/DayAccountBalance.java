88504b56aaf0093b4418d0e301097ac2a4480a5c
Mehmet Tosun <mtosun@student.ethz.ch>
2026-07-17 11:58:51 +0200

    add updateAmount()

--- src/main/java/expenseTracker/model/DayAccountBalance.java
+++ src/main/java/expenseTracker/model/DayAccountBalance.java
@@ -1,10 +1,11 @@
 package expenseTracker.model;
 
+import java.math.BigDecimal;
 import java.util.*;
 
 public class DayAccountBalance extends AccountBalance {
-    // By default, expenses are initialized to 0
-    int expenses = 0;
+    // By default, total amount of expenses are initialized to 0
+    BigDecimal amount = BigDecimal.ZERO;
 
     // Creates map from unique expense id's to its expense
     Map<Long, Expense> expenseMap = new HashMap<Long, Expense>();
@@ -15,5 +16,16 @@ public class DayAccountBalance extends AccountBalance {
 
     public void removeExpense(Expense e) {
         expenseMap.remove(e.getUniqueID());
+    }
+
+    public void updateAmount() {
+        BigDecimal currAmount = BigDecimal.ZERO;
+
+        for (Long l : expenseMap.keySet()) {
+            Expense currExpense = expenseMap.get(l);
+            currAmount = currAmount.add(currExpense.getAmount());
+        }
+
+        amount = currAmount;
     }
 }
