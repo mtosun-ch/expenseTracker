5f2b50b6d6f01a401ace71559b0a8f3dad076929
Mehmet Tosun <mtosun@student.ethz.ch>
2026-07-24 15:11:03 +0200

    add updateExpense

--- src/main/java/expenseTracker/controller/ExpenseController.java
+++ src/main/java/expenseTracker/controller/ExpenseController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +40,17 @@ public class ExpenseController {
     public String deleteExpense(@PathVariable("id") long id) {
         expenseRepository.deleteById(id);
         return "Expense with ID " + id + " deleted successfully.";
+    }
+
+    @PutMapping("/{id}")
+    public String updateExpense(@PathVariable("id") long id, @RequestBody Expense e) {
+        try {
+            Expense currExpense = expenseRepository.findById(id).get();
+            expenseRepository.save(currExpense);
+            return "Expense with ID " + id + " updated successfully.";
+        } catch(Exception e1) {
+            return "Expense with ID " + id + " does not exist.";
+        }
     }
 
 }
