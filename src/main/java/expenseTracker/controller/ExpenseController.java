3de6564721264a39c30e5e47bf52305f4f15cebe
Mehmet Tosun <mtosun@student.ethz.ch>
2026-07-24 14:56:05 +0200

    add deleteExpense

--- src/main/java/expenseTracker/controller/ExpenseController.java
+++ src/main/java/expenseTracker/controller/ExpenseController.java
@@ -1,6 +1,8 @@
 package expenseTracker.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +35,10 @@ public class ExpenseController {
         return "Expense with ID " + saved.getUniqueID() + " added successfully.";
     }
 
+    @DeleteMapping("/{id}")
+    public String deleteExpense(@PathVariable("id") long id) {
+        expenseRepository.deleteById(id);
+        return "Expense with ID " + id + " deleted successfully.";
+    }
 
 }
