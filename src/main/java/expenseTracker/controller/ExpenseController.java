b6118c6445c89e6bb4fa2fa818e52a8f5f9d7d63
Mehmet Tosun <mtosun@student.ethz.ch>
2026-07-24 16:51:59 +0200

    fix deleteExpense

--- src/main/java/expenseTracker/controller/ExpenseController.java
+++ src/main/java/expenseTracker/controller/ExpenseController.java
@@ -1,5 +1,7 @@
 package expenseTracker.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -37,13 +39,12 @@ public class ExpenseController {
     }
 
     @DeleteMapping("/{id}")
-    public String deleteExpense(@PathVariable("id") long id) {
+    public ResponseEntity<String> deleteExpense(@PathVariable("id") long id) {
         if (expenseRepository.findById(id).isEmpty()) {
-            return "Expense with ID " + id + " does not exist.\n";
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Expense with ID " + id + " does not exist.\n");
         }
-
         expenseRepository.deleteById(id);
-        return "Expense with ID " + id + " deleted successfully.\n";
+        return ResponseEntity.status(HttpStatus.OK).body("Expense with ID " + id + " deleted successfully.\n");
     }
 
     @PutMapping("/{id}")
